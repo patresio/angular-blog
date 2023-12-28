9b256623bb8c9ed8b0fd405e138ffec22ed0c805
Ricardo Pereira <patresio@gmail.com>
2023-12-28 15:36:06 -0300

    components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover:string=""
+  @Input()
+  photoCoverAlt:string=""
+  @Input()
+  cardTitle:string=""
+  @Input()
+  cardAuthor:string=""
+  @Input()
+  cardLink:string=""
+  @Input()
+  cardDescription:string=""
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
-
+  @Input()
+  photoCover:string=""
+  @Input()
+  photoCoverAlt:string=""
+  @Input()
+  cardAuthor:string=""
+  @Input()
+  cardTitle:string=""
+  @Input()
+  cardLink:string=""
 }
